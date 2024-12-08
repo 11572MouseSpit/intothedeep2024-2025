dea2d8ed719cbc7801efcbe157a69e3baa4c15b9
ellsbells0123 <elanorstarflower126@gmail.com>
2024-12-08 09:44:19 -0600

    copy params

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumDrive.java
@@ -489,3 +489,30 @@ public final class MecanumDrive {
         );
     }
 }
+/**
+ * Params from Black Friday
+ *
+ * // feedforward parameters (in tick units)
+ *         public double kS = 1.9883064224432494;
+ *         public double kV = 0.00026505829803169506;
+ *         public double kA = 0.0000721;
+ *
+ *         // path profile parameters (in inches)
+ *         public double maxWheelVel = 50;
+ *         public double minProfileAccel = -30;
+ *         public double maxProfileAccel = 50;
+ *
+ *         // turn profile parameters (in radians)
+ *         public double maxAngVel = Math.PI; // shared with path
+ *         public double maxAngAccel = Math.PI;
+ *
+ *         // path controller gains
+ *         public double axialGain = 4;
+ *         public double lateralGain = 3;
+ *         public double headingGain = 2; // shared with turn
+ *
+ *         public double axialVelGain = 0.0;
+ *         public double lateralVelGain = 0.0;
+ *         public double headingVelGain = 0.0; // shared with turn
+ *     }
+ */
